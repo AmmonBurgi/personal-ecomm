c755165595fce2ba9da8e47932155413c05b0986
Ammon Burgi <ammonburgi@Ammons-MacBook-Pro.local>
2020-06-15 12:37:11 -0600

    header

---
+++ server/index.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+const express = require('express'),
+    session = require('express-session'),
+    massive = require('massive'),
+    {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET} = process.env,
+    port = SERVER_PORT,
+    app = express()
+    
+    app.use(express.json())
+
+    app.use(session({
+        resave: false,
+        saveUninitialized: true,
+        secret: SESSION_SECRET,
+        cookie: {maxAge: 1000 * 60 * 60 * 24 * 7}
+    }))
+
+    massive({
+        connectionString: CONNECTION_STRING,
+        ssl: {rejectUnauthorized: false}
+    }).then(db => {
+        app.set('db', db)
+        console.log('db connected')
+        app.listen(port, () => console.log(`server listening on port ${port}`))
+    })
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import styled from 'styled-components'
+import {Link} from 'react-router-dom'
+
+const NavBar = styled('header')`
+display: flex;
+justify-content: center;
+align-items: center;
+width: 100vw;
+height: 10vh;
+border-bottom: 1px solid black;
+`
 
 function Header(){
     return(
-        <div>
-            Header Component
-        </div>
+        <NavBar>
+            <Link><button>Login</button></Link>
+        </NavBar>
     )
 }
 export default Header
