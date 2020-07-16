42b2263a62b5cec5e150627a4da10dd622014d73
Ammon Burgi <ammonburgi@Ammons-MacBook-Pro.local>
2020-07-15 22:13:47 -0600

    stripe successful

--- src/App.js
+++ src/App.js
@@ -5,17 +5,20 @@ import Header from './Components/Header/Header'
 import {ToastContainer} from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import './App.css';
+import {Elements} from '@stripe/react-stripe-js'
+import {loadStripe} from '@stripe/stripe-js'
 
-
-
+const stripePromise = loadStripe('pk_test_51GxfLDIvVVGRQ9wLZdeKLkEieEKliFGS6yGGwLt2b0jHmVzlbSruFeMbzTX6ABfoZb1u2bX4legnRHxhn08QDbYf00iUQC9LPn')
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      {routes}
-      <ToastContainer autoClose={2100} />
-    </div>
+    <Elements stripe={stripePromise}>
+      <div className="App">
+        <Header />
+        {routes}
+        <ToastContainer autoClose={2100} />
+      </div>
+    </Elements>
   );
 }
 
