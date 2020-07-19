dd34ae295677293f8827bd955e258b0594ee8a76
Ammon Burgi <ammonburgi@Ammons-MacBook-Pro.local>
2020-07-18 20:08:03 -0600

    small changes

--- server/index.js
+++ server/index.js
@@ -9,6 +9,8 @@ const express = require('express'),
     port = SERVER_PORT,
     app = express()
 
+    const path = require('path');
+
     app.use(express.json())
     app.use(express.static(__dirname + '/../build'))
 
